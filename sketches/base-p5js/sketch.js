06732c632c6681a6fdef87abb57744da8f7c8fcd
Sandro Miccoli <smiccoli@gmail.com>
2021-03-02 18:59:54 +0100

    Adding base p5.js sketch

---
+++ sketches/base-p5js/sketch.js
@@ -0,0 +1,14 @@
+function setup(){
+  var cnv = createCanvas(displayWidth, displayHeight);
+  cnv.position(0,0);
+  cnv.style('position', 'fixed');
+  cnv.style('display', 'block');
+  cnv.style('z-index','-1'); // thanks Shiffman: https://www.youtube.com/watch?v=OIfEHD3KqCg
+}
+
+function draw(){
+  background(244);
+  fill(0);
+  stroke(255,0,0)
+  ellipse(mouseX,mouseY,50,50);
+}
