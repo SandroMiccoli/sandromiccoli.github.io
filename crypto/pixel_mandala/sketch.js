0260d2a4dfcc7187ac407482ea65a919a584d05c
Sandro Miccoli <smiccoli@gmail.com>
2021-03-21 21:29:03 +0100

    Updating Pixel Mandala page

---
+++ crypto/pixel_mandala/sketch.js
@@ -0,0 +1,117 @@
+// Pixel Mandala | 64x64
+// by @sandromiccoli
+// 21/03/2021
+
+const canvas = 750;
+const full_size = 64;
+const pixel = canvas/full_size;
+const half_size = full_size/2;
+
+let quarter_matrix = new Array(half_size).fill(0).map(() => new Array(half_size).fill(0));
+let quarter_matrix_from = new Array(half_size).fill(0).map(() => new Array(half_size).fill(0));
+let quarter_matrix_to = new Array(half_size).fill(0).map(() => new Array(half_size).fill(0));
+const seed = 'tz1Q4r6CMMNRvvQBZUFU5za4aTFZpC43rX8G'
+
+let lerp = 0;
+let inc = 0.05;
+let lerp_color = false;
+let cnv;
+
+function setup(){
+  cnv = createCanvas(canvas, canvas);
+  cnv.parent("live");
+
+  noiseSeed(seed.hashCode());
+  for (var row = 0; row < half_size; row++) 
+    for (var col = 0; col < half_size; col++){
+      quarter_matrix[col][row] = noise(tan(row*col)+row*col/250)*320;
+      quarter_matrix[col][row] = (quarter_matrix[col][row]+noise(sin(row*col)+row*col/250)*100)%320;
+      quarter_matrix[col][row] = (quarter_matrix[col][row]>140) ? map(quarter_matrix[col][row],0,320,100,240) : map(quarter_matrix[col][row],0,320,25,50);
+    }
+
+  generateNextColor();
+
+  colorMode(HSB,255);
+  smooth();
+  noStroke();    
+}
+
+function draw(){
+  for (var row = 0; row < full_size; row++) {
+    for (var col = 0; col < full_size; col++) {
+      if (row < half_size && col < half_size)
+        fill(quarter_matrix[col][row],255,220);
+      else if (row < half_size && col >= half_size)
+        fill(quarter_matrix[(full_size-1)-col][row],255,220);
+      else if (row >= half_size && col < half_size)
+        fill(quarter_matrix[col][(full_size-1)-row],255,220);
+      else if (row >= half_size && col >= half_size)
+        fill(quarter_matrix[(full_size-1)-col][(full_size-1)-row],255,220);
+
+      rect(pixel*col,pixel*row,pixel,pixel);
+    }
+  }
+  lerpColors();
+}
+
+function generateNextColor(){
+  quarter_matrix_from = quarter_matrix;
+
+  noiseSeed(random(999999999));
+  for (var row = 0; row < half_size; row++) 
+    for (var col = 0; col < half_size; col++){
+      quarter_matrix_to[col][row] = noise(tan(row*col)+row*col/250)*320;
+      quarter_matrix_to[col][row] = (quarter_matrix_to[col][row]+noise(sin(row*col)+row*col/250)*100)%320;
+      quarter_matrix_to[col][row] = (quarter_matrix_to[col][row]>140) ? map(quarter_matrix_to[col][row],0,320,100,240) : map(quarter_matrix_to[col][row],0,320,25,50);
+    }
+}
+
+function lerpColors(){
+  if(lerp_color){
+
+    quarter_matrix_from = quarter_matrix;
+
+    for (var row = 0; row < half_size; row++) {
+      for (var col = 0; col < half_size; col++) {
+        quarter_matrix[col][row] = lerpColor(color(quarter_matrix_from[col][row],255,220),color(quarter_matrix_to[col][row],255,220),lerp);
+      }
+    }
+  
+  
+    lerp+=inc;
+    if (lerp > 1){
+      lerp_color = false;
+      generateNextColor();
+      lerp=0;
+      console.log("Lerp finished");
+    }
+  }
+}
+
+function mouseClicked(){
+  lerp_color = true;
+}
+
+// source: https://werxltd.com/wp/2010/05/13/javascript-implementation-of-javas-string-hashcode-method/
+String.prototype.hashCode = function(){
+  var hash = 0;
+  if (this.length == 0) return hash;
+  for (i = 0; i < this.length; i++) {
+    char = this.charCodeAt(i);
+    hash = ((hash<<5)-hash)+char;
+    hash = hash & hash; // Convert to 32bit integer
+  }
+  return hash;
+}
+
+
+window.onresize = function() {
+  canvas.size(windowWidth, windowHeight);
+};
+
+function keyTyped() {
+  if (key === 's') {
+    saveCanvas(cnv, 'horizontalLines', 'png');
+  }
+}
+
